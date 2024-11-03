817fbdc68525befbadecf52b7967628432f6bce5
Amarjeet Prajapati <amarjeet275303@gmail.com>
2024-11-03 12:29:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl bg-gray-500">Vite + React</h1>
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
