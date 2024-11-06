2b62321ae39cc1dd9e783ac871d265a1a21cd6b8
Amarjeet Prajapati <amarjeet275303@gmail.com>
2024-11-06 23:12:54 +0530

    header updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux"
 
 import store from "./utils/store"
 
-import { createBrowserRouter,Outlet, RouterProvider } from "react-router-dom"
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
 import MainContainer from "./MainContainer"
 import WatchPage from "./WatchPage"
 
